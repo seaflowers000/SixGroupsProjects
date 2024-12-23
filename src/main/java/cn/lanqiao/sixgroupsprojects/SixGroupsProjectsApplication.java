5e5859f06cda5a3919544fca7ae060bf32859610
邹金亮 <2774278338@qq.com>
2024-12-23 17:12:18 +0800

    init

---
+++ src/main/java/cn/lanqiao/sixgroupsprojects/SixGroupsProjectsApplication.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.sixgroupsprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixGroupsProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixGroupsProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lanqiao/sixgroupsprojects/SixGroupsProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.sixgroupsprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixGroupsProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
