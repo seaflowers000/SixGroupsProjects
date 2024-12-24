bc19aff78a04e3a8cd59cc8905ede49c8c4b1e44
54454 <544548586@qq.com>
2024-12-24 09:52:34 +0800

    yml

--- src/main/java/cn/lanqiao/sixgroupsprojects/SixGroupsProjectsApplication.java
+++ src/main/java/cn/lanqiao/sixgroupsprojects/SixGroupsProjectsApplication.java
@@ -1,9 +1,11 @@
 package cn.lanqiao.sixgroupsprojects;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.lanqiao.sixgroupsprojects.mapper")
 public class SixGroupsProjectsApplication {
 
     public static void main(String[] args) {
